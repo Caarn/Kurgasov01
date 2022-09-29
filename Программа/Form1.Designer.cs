c49f704fa770331b26c43e26b3399754c5e798b3
Caarn <91573894+Caarn@users.noreply.github.com>
2022-09-29 10:30:55 +0300

    Add files via upload

---
+++ Программа/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace Kurgasov01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Технический",
+            "Гуманитарный",
+            "Естественник"});
+            this.comboBox1.Location = new System.Drawing.Point(24, 125);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(210, 28);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 96);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Склад ума";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(20, 157);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(214, 46);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Предмет в котором ты разбираешься";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(24, 207);
+            this.comboBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(210, 28);
+            this.comboBox2.TabIndex = 3;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(20, 239);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(195, 46);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Кем ты планируешь работать";
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(24, 289);
+            this.comboBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(210, 28);
+            this.comboBox3.TabIndex = 5;
+            this.comboBox3.SelectedIndexChanged += new System.EventHandler(this.comboBox3_SelectedIndexChanged);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.Location = new System.Drawing.Point(259, 157);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(301, 234);
+            this.richTextBox1.TabIndex = 6;
+            this.richTextBox1.Text = "";
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(255, 107);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(305, 46);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Специальность, которая тебе подходит";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(572, 403);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "1 Лабораторная";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Программа/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Kurgasov01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            comboBox2.Items.Clear();
+            comboBox3.Items.Clear();
+            richTextBox1.Clear();
+            switch (comboBox1.Text)
+            {
+                case "Технический":
+                    comboBox2.Items.Add("Физика");
+                    comboBox2.Items.Add("Математика");
+                    comboBox2.Items.Add("Информатика");
+                    break;
+                case "Гуманитарный":
+                    comboBox2.Items.Add("Творчество");
+                    comboBox2.Items.Add("Работа с детьми");
+                    comboBox2.Items.Add("Общество");
+                    comboBox2.Items.Add("Лингвист");
+                    break;
+                case "Естественник":
+                    comboBox2.Items.Add("Химия");
+                    comboBox2.Items.Add("Биология");
+                    comboBox2.Items.Add("Физическая культура");
+                    break;
+            }
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            comboBox3.Items.Clear();
+            richTextBox1.Clear();
+            switch (comboBox2.Text)
+            {
+                case "Физика":
+                    comboBox3.Items.Add("Электротехник");
+                    break;
+                case "Математика":
+                    comboBox3.Items.Add("Металлург");
+                    comboBox3.Items.Add("Инженер");
+                    comboBox3.Items.Add("Программист-аналитик");
+                    comboBox3.Items.Add("Геодезист");
+                    break;
+                case "Информатика":
+                    comboBox3.Items.Add("Программист-аналитик");
+                    comboBox3.Items.Add("Аналитик");
+                    comboBox3.Items.Add("Программист-разработчик");
+                    break;
+                case "Творчество":
+                    comboBox3.Items.Add("Художник");
+                    comboBox3.Items.Add("Музыкант");
+                    break;
+                case "Работа с детьми":
+                    comboBox3.Items.Add("Учитель");
+                    comboBox3.Items.Add("Воспитатель");
+                    break;
+                case "Лингвист":
+                    comboBox3.Items.Add("Лингвист");
+                    break;
+                case "Обществознание":
+                    comboBox3.Items.Add("Аналитик");
+                    comboBox3.Items.Add("Администратор");
+                    comboBox3.Items.Add("Социальный работник");
+                    comboBox3.Items.Add("Историк");
+                    break;
+                case "Химия":
+                    comboBox3.Items.Add("Химик");
+                    comboBox3.Items.Add("Металлург");
+                    break;
+                case "Биология":
+                    comboBox3.Items.Add("Косметолог");
+                    comboBox3.Items.Add("Врач");
+                    break;
+                case "Физическая культура":
+                    comboBox3.Items.Add("Военный");
+                    comboBox3.Items.Add("Спортсмен");
+                    break;
+            }
+        }
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            switch (comboBox3.Text)
+            {
+                case "Электротехник":
+                    richTextBox1.Text = "Вам подходит специальность:\n- Электроэнергетика и электротехника(ЛГТУ)";
+                    break;
+                case "Металлург":
+                    richTextBox1.Text = "Вам подходят специальности:\n- Материаловедение и технологии материалов(ЛГТУ)\n- Металлургия(ЛГТУ)";
+                    break;
+                case "Инженер":
+                    richTextBox1.Text = "Вам подходят специальности:\n- Строительство(ЛГТУ)\n- Градостроительство(ЛГТУ)";
+                    break;
+                case "Программист-аналитик":
+                    richTextBox1.Text = "Вам подходит специальность:\n- Прикладная математика(ЛГТУ)"; 
+                    break;
+                case "Геодезист":
+                    richTextBox1.Text = "Вам подходит специальность:\n- Геодезист(СПБГУ)";
+                    break;
+                case "Аналитик":
+                    richTextBox1.Text = "Вам подходит специальность:\n- Администрирование информационных систем(ЛГТУ)";
+                    break;
+                case "Программист-разработчик":
+                    richTextBox1.Text = "Вам подходят специальности:\n- Информатика и вычислительная техника(ЛГТУ)\n- Администрирование информационных систем(ЛГТУ)\n- Программная инженерия(ЛГТУ)";
+                    break;
+                case "Художник":
+                    richTextBox1.Text = "Вам подходят специальности:\n- Живопись(ЛГПУ)\n- Дизайнерство(ЛГТУ)";
+                    break;
+                case "Музыкант":
+                    richTextBox1.Text = "Вам подходят специальности:\n- Пение(ВГИИ)\n- Пианино(ВГИИ)";
+                    break;
+                case "Учитель":
+                    richTextBox1.Text = "Вам подходят специальности:\n- Учитель русского языка(ЛГПУ)\n- Учитель математики(ЛГПУ)\n- Учитель информатики(ЛГПУ)";
+                    break;
+                case "Воспитатель":
+                    richTextBox1.Text = "Вам подходит специальность:\n- Учитель дошкольного образования(ЛГПУ)";
+                    break;
+                case "Лингвист":
+                    richTextBox1.Text = "Вам подходит специальность:\n- Учитель иностранного языка(ЛГПУ)";
+                    break;
+                case "Администратор":
+                    richTextBox1.Text = "Вам подходит специальность:\n- Управление персоналом(ЛГТУ)";
+                    break;
+                case "Социальный работник":
+                    richTextBox1.Text = "Вам подходят специальности:\n- Психология(ЛГТУ)\n- Социология(ЛГТУ)";
+                    break;
+                case "Историк":
+                    richTextBox1.Text = "Вам подходят специальности:\n- Правоохранительная деятельность(ИМВД)\n- Документоведение и архивоведение(ЛГТУ)";
+                    break;
+                case "Химик":
+                    richTextBox1.Text = "Вам подходят специальности:\n- Химия(ЛГТУ)\n- Химическая технология(ЛГТУ)";
+                    break;
+                case "Косметолог":
+                    richTextBox1.Text = "Вам подходит специальность:\n- Косметология(ФГБОУ)";
+                    break;
+                case "Врач":
+                    richTextBox1.Text = "Вам подходят специальности:\n- Сестринское дело(ФГБОУ)\n- Медико-профилактическое дело(ФГБОУ)";
+                    break;
+                case "Военный":
+                    richTextBox1.Text = "Вам подходят специальности:\n- Управление беспилотниками(ВУНЦ)\n- Военные науки(ВУНЦ)";
+                    break;
+                case "Спортсмен":
+                    richTextBox1.Text = "Вам подходят специальности:\n- Рекреация(ЛГПУ)\n- Спорт(ЛГПУ)";
+                    break;
+            }
+        }
+    }
+}
