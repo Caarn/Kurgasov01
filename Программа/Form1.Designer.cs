911d2eac09e08b39f3680e7a41fdf71f05082377
Caarn <koretnikov01@mail.ru>
2023-02-13 09:38:35 +0300

    трролдл

--- Программа/Form1.Designer.cs
+++ Программа/Form1.Designer.cs
@@ -106,9 +106,9 @@
             this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.richTextBox1.Location = new System.Drawing.Point(259, 58);
+            this.richTextBox1.Location = new System.Drawing.Point(259, 183);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(301, 333);
+            this.richTextBox1.Size = new System.Drawing.Size(301, 208);
             this.richTextBox1.TabIndex = 6;
             this.richTextBox1.Text = "";
             // 
